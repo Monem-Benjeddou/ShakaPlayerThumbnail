2c7da0afd9242176b91556775716759e52c199fb
Monem <monem1bouazizi@gmail.com>
2024-08-22 10:56:43 +0100

    fixed the thumbnail x positioning

--- ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
+++ ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54bc848e394a305cf7870ec0187eb2b6ff82413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c576dbd54349831c8091771669f6d90f1d820b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
