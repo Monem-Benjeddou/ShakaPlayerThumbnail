c0f92e145e153c1ff4fde4a6b878b4d5ec4b3e99
Monem <monem1bouazizi@gmail.com>
2024-08-27 16:46:13 +0100

    fixing the ui

--- ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
+++ ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0edc59b60fad4d76d23bf71d07035176f40d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2577cb0c5221885c7a218f32f6fec1b989db46fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
