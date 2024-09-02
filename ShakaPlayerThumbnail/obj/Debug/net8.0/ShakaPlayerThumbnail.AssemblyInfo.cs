87c585e7e6221ec5b1eba85790378bddbc7f51c4
Monem <monem1bouazizi@gmail.com>
2024-09-02 13:33:10 +0100

    fixed some stuff with the responsiveness

--- ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
+++ ShakaPlayerThumbnail/obj/Debug/net8.0/ShakaPlayerThumbnail.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c891b0297db54647b4862e447b91e6e8bd2d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e5bf24e5047b92d8f85b21df680ea6f7efad12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShakaPlayerThumbnail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
