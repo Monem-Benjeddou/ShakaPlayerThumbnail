9c6116aea363160e6d234cf29bb3e52f61460664
Monem <monem1bouazizi@gmail.com>
2024-09-17 14:01:17 +0100

    fixed the ui uplaod

--- ShakaPlayerThumbnail/Tools/FfmpegTool.cs
+++ ShakaPlayerThumbnail/Tools/FfmpegTool.cs
@@ -21,17 +21,21 @@ namespace ShakaPlayerThumbnail.Tools
             int numberOfTiles = (int)Math.Ceiling((double)totalFrames / framesPerTile);
             var thumbnailInfo = new List<ThumbnailInfo>();
 
+            // Ensure output path exists
+            if (!Directory.Exists(outputImagePath))
+                Directory.CreateDirectory(outputImagePath);
+
             for (int i = 1; i <= numberOfTiles; i++)
             {
                 double startTime = (i - 1) * framesPerTile * intervalSeconds;
-                double endTime = Math.Min(startTime + framesPerTile * intervalSeconds, videoDuration);
+                double duration = Math.Min(framesPerTile * intervalSeconds, videoDuration - startTime);
                 int framesInThisSection = Math.Min(totalFrames - (i - 1) * framesPerTile, framesPerTile);
 
-                var arguments = BuildFfmpegArguments(videoPath, startTime, endTime, outputImagePath, i, intervalSeconds,videoName);
+                var arguments = BuildFfmpegArguments(videoPath, startTime, duration, outputImagePath, i, intervalSeconds,videoName);
                 await RunFFmpeg(arguments);
 
-                thumbnailInfo.Add(new ThumbnailInfo(i, startTime, endTime, framesInThisSection));
-                
+                thumbnailInfo.Add(new ThumbnailInfo(i, startTime, startTime + duration, framesInThisSection));
+
                 int progress = (i * 100) / numberOfTiles;
                 reportProgress(progress); 
             }
@@ -39,11 +43,10 @@ namespace ShakaPlayerThumbnail.Tools
             GenerateVttFile(videoName, thumbnailInfo, intervalSeconds, tileWidth, tileHeight);
         }
 
-        private static string BuildFfmpegArguments(string videoPath, double startTime, double endTime, string outputImagePath, int tileIndex, int intervalSeconds, string videoName) =>
-            $"-i \"{videoPath}\" -ss {startTime} -t {endTime - startTime} " +
-            $"-vf \"select=not(mod(t\\,{intervalSeconds})),scale=120:-1,tile=10x10,fps=1\" " +
-            $"-quality 40 -compression_level 4 -threads 2 -y \"{outputImagePath}/{videoName}{tileIndex}.webp\"";
-
+        private static string BuildFfmpegArguments(string videoPath, double startTime, double duration, string outputImagePath, int tileIndex, int intervalSeconds,string videoName) =>
+            $"-ss {startTime} -i \"{videoPath}\" -t {duration} " +
+            $"-vf \"select=not(mod(t\\,{intervalSeconds})),scale=120:-1,tile=10x10\" " +
+            $"-quality 50 -compression_level 6 -threads 0 -y \"{Path.Combine(outputImagePath,videoName, $"{tileIndex}.webp")}\"";
 
         private static double GetVideoDuration(string videoPath)
         {
@@ -55,7 +58,6 @@ namespace ShakaPlayerThumbnail.Tools
             }
             return duration;
         }
-
 
         private static async Task RunFFmpeg(string arguments)
         {
@@ -99,7 +101,7 @@ namespace ShakaPlayerThumbnail.Tools
                     FileName = fileName,
                     Arguments = arguments,
                     RedirectStandardOutput = true,
-                    RedirectStandardError = true, 
+                    RedirectStandardError = true,
                     UseShellExecute = false,
                     CreateNoWindow = true
                 }
@@ -115,14 +117,13 @@ namespace ShakaPlayerThumbnail.Tools
             await process.WaitForExitAsync();
         }
 
-
         private static void GenerateVttFile(string videoName, List<ThumbnailInfo> thumbnailInfo, int intervalSeconds, int tileWidth, int tileHeight)
         {
             var previewDirectory = $"/etc/data/previews/{videoName}";
             if (!Directory.Exists(previewDirectory))
                 Directory.CreateDirectory(previewDirectory);
             var vttFilePath = Path.Combine(previewDirectory, $"{videoName}.vtt");
-            //var vttFilePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","previews", $"{videoName}.vtt");
+
             using var writer = new StreamWriter(vttFilePath);
             writer.WriteLine("WEBVTT");
 
@@ -134,8 +135,6 @@ namespace ShakaPlayerThumbnail.Tools
 
                     writer.WriteLine($"{TimeSpan.FromSeconds(startTime):hh\\:mm\\:ss\\.fff} --> {TimeSpan.FromSeconds(endTime):hh\\:mm\\:ss\\.fff}");
                     writer.WriteLine($"/data/previews/{videoName}/{videoName}{info.TileIndex}.webp#xywh={xOffset},{yOffset},120,68");
-                    //writer.WriteLine($"/previews/{videoName}{info.TileIndex}.webp#xywh={xOffset},{yOffset},120,68");
-
                     writer.WriteLine();
                 }
             }
@@ -152,6 +151,7 @@ namespace ShakaPlayerThumbnail.Tools
             return (startTime, endTime, xOffset, yOffset);
         }
     }
+
     public class ThumbnailInfo
     {
         public int TileIndex { get; }
