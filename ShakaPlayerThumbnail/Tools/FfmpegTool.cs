ccaf85214c4a2725ad97710006408141a817f979
Monem <monem1bouazizi@gmail.com>
2024-09-26 15:49:42 +0100

    fixed the same thing in the previous one

--- ShakaPlayerThumbnail/Tools/FfmpegTool.cs
+++ ShakaPlayerThumbnail/Tools/FfmpegTool.cs
@@ -25,9 +25,10 @@ namespace ShakaPlayerThumbnail.Tools
             var width = image.Width;
             var height = image.Height;
             if (image.Width > image.Height)
-                width = -10;
+                width -= 10;
             else
-                height = -10;
+                height -= 10;
+            Console.WriteLine("width: "+width+"height: "+height);
             return (width / 10, height / 10);
         }
 
