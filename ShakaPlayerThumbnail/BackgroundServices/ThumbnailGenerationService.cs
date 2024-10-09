3d1a71b27b1716537b83d8b047cf2ea34fb6479b
Monem <monem1bouazizi@gmail.com>
2024-10-09 16:42:29 +0100

    fixed the concurrency problem

--- ShakaPlayerThumbnail/BackgroundServices/ThumbnailGenerationService.cs
+++ ShakaPlayerThumbnail/BackgroundServices/ThumbnailGenerationService.cs
@@ -9,42 +9,28 @@ public class ThumbnailGenerationService(
     IProgressTracker progressTracker)
     : BackgroundService
 {
-    private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(2); 
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Thumbnail Generation Service is starting.");
-        var runningTasks = new List<Task>();
-
+        
         while (!stoppingToken.IsCancellationRequested)
         {
             var workItem = await taskQueue.DequeueAsync(stoppingToken);
+            var taskId = Guid.NewGuid().ToString();
 
-            var task = Task.Run(async () =>
+            try
             {
-                var taskId = Guid.NewGuid().ToString();
-                progressTracker.SetProcessingStatus(taskId, true);
-
-                await _semaphoreSlim.WaitAsync(stoppingToken);
-                try
-                {
-                    await workItem(stoppingToken);
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Error occurred executing thumbnail generation task.");
-                }
-                finally
-                {
-                    _semaphoreSlim.Release();
-                    progressTracker.SetProcessingStatus(taskId, false);
-                }
-            }, stoppingToken);
-
-            runningTasks.Add(task);
+                await workItem(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred executing thumbnail generation task.");
+            }
+            finally
+            {
+                progressTracker.SetProcessingStatus(taskId, false);
+            }
         }
-
-        await Task.WhenAll(runningTasks);
 
         logger.LogInformation("Thumbnail Generation Service is stopping.");
     }
